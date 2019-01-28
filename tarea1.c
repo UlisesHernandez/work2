121df506f67696f13e91cb7f423e3d776ff9d541
ulises <ulisesdejesusk9999@hotmail.com>
2019-01-28 13:37:23 -0600

    Este archivo es la tarea 1 de programacion prueva 2

---
+++ tarea1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int op1,op2,res;
+	printf( "operador 1 :" );
+	scanf("%i",&op1);
+	printf( "operador 2:." );
+	scanf("%i",&op2);
+	res=op1+op2;
+		printf( "la suma de %i ",res);
+	
+	
+	return 0;
+	
+	
+	
+}
